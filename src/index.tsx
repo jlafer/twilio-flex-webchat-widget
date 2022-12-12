68974b4e407dfa6dd18d6a2814101edda1293b5c
John Lafer <jlafer@yahoo.com>
2022-12-12 15:52:19 -0800

    index.tsx now just provides exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,5 @@
-import { render } from "react-dom";
-import { Provider } from "react-redux";
-
 import webchatInit from "./webchat";
-import configureStore, { StoreWithInjector } from "./store/configureStore";
-import { WebchatWidget } from "./components/WebchatWidget";
-import { ConfigState } from "./store/definitions";
-import { initLogger } from "./logger";
+import {WebchatWidget} from './components/WebchatWidget';
 
-const initWebchat = async (config: ConfigState) => {
-    initLogger();
-    const store = configureStore({});
-    webchatInit(config, store);
-    const rootElement = document.getElementById("twilio-webchat-widget-root");
-
-    render(
-        <Provider store={store}>
-            <WebchatWidget />
-        </Provider>,
-        rootElement
-    );
-
-    if (window.Cypress) {
-        window.store = store;
-    }
-};
-
-declare global {
-    interface Window {
-        Twilio: {
-            initWebchat: (config: ConfigState) => void;
-        };
-        Cypress: Cypress.Cypress;
-        store: StoreWithInjector;
-    }
-}
-
-// Expose `initWebchat` function to window object
-Object.assign(window, {
-    Twilio: {
-        initWebchat
-    }
-});
+export {WebchatWidget};
+export default webchatInit;
